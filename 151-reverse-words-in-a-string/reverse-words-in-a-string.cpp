0ec2ff002113e85cdec982d269ac783b976a9c5a
Sabarish M N <74953032+SabarishMN@users.noreply.github.com>
2025-08-29 12:07:22 -0400

    Time: 19 ms (5.73%) | Memory: 50.4 MB (5.36%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,40 +2,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // string str;
-        // vector<string> v;
-        // stringstream ss(s);
-
-        // while(getline(ss,s,' ')) {
-        //     v.push_back(s);
-        // }
-
-        // string ans = "";
-        // for(int i = v.size()-1;i>=0;i--)
-        // {
-        //     if()
-        //     ans = ans + v[i] + " ";
-        //     // if(i = 0) ans = ans + " ";
-        // }
-
-        // return ans.substr(0, ans.length()-1);
-
-        if(s.empty()) return "";
+        string str;
         vector<string> v;
-        string str = "";
-        for(int i=0;i<s.length();i++){
-            if(s[i] != ' ') {
-                str = str + s[i];
-            } else { 
-                if(!str.empty()) {v.push_back(str); str= "";}}
+        istringstream ss(s);
+        string word;
+        while(ss >> word) {
+            v.push_back(word);
         }
 
-        if(!str.empty()) {
-            v.push_back(str);
-        }
-
-        string ans="";
-
+        string ans = "";
         for(int i = v.size()-1;i>=0;i--)
         {
             ans = ans + v[i] + " ";
@@ -43,6 +18,30 @@ public:
         }
 
         return ans.substr(0, ans.length()-1);
+
+        // if(s.empty()) return "";
+        // vector<string> v;
+        // string str = "";
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i] != ' ') {
+        //         str = str + s[i];
+        //     } else { 
+        //         if(!str.empty()) {v.push_back(str); str= "";}}
+        // }
+
+        // if(!str.empty()) {
+        //     v.push_back(str);
+        // }
+
+        // string ans="";
+
+        // for(int i = v.size()-1;i>=0;i--)
+        // {
+        //     ans = ans + v[i] + " ";
+        //     // if(i = 0) ans = ans + " ";
+        // }
+
+        // return ans.substr(0, ans.length()-1);
 
 
     }
