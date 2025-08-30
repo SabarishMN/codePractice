168cfacbe97c286558ee0d2baf31f0724e4f6d58
Sabarish M N <74953032+SabarishMN@users.noreply.github.com>
2025-08-30 10:18:48 -0400

    Time: 3 ms (51.28%) | Memory: 73.8 MB (15.35%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.cpp
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.cpp
@@ -5,15 +5,23 @@ public:
         int max = 0;
         vector<int> ans;
 
+        stack<int> st;
+
         for(int i=heights.size()-1;i>=0;i--)
         {
             if(max < heights[i]) {
-                 ans.push_back(i);
+                st.push(i);
+                //  ans.push_back(i);
                 max = heights[i];
             }
         }
 
-        sort(ans.begin(), ans.end());
+        // sort(ans.begin(), ans.end());
+
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
 
         return ans;
         
